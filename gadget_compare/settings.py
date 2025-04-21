a2c902923bb4b20e19919b0d262e0a2ca1cbc59a
Anuj <anujofficial0990@gmail.com>
2025-04-21 17:18:04 +0530

    Static file resolved

--- gadget_compare/settings.py
+++ gadget_compare/settings.py
@@ -84,9 +84,9 @@ DATABASES = {
 }
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
+# STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
